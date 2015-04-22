0b02d0652b2b2a71dc5e656361a57af30b8d832d
pallavi <pallavi.snehal@accenture.com>
2015-04-22 15:17:20 +0530

    commit

---
+++ HelloWorld/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\pallavi.snehal\git\HelloWorld_Jenkins_Perfecto\HelloWorld\bin\AndroidManifest.xml : \
+C:\Users\pallavi.snehal\git\HelloWorld_Jenkins_Perfecto\HelloWorld\AndroidManifest.xml \
